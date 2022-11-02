e3a803e9b85e548c0fc61a5031123f7f3e6e48c0
imajeason <imajeason@gmail.com>
2022-11-02 16:48:26 +0800

    fix

--- NaiveProxy/do.sh
+++ NaiveProxy/do.sh
@@ -4,8 +4,8 @@ shell_renew(){
     curl -o /root/.naive.sh https://raw.githubusercontent.com/imajeason/nas_tools/main/NaiveProxy/install.sh 
     chmod +x /root/.naive.sh
     ln -s /root/.naive.sh /usr/bin/naive
+    echo
+    echo "naive 命令安装完毕，请使用naive进行操作。"
 }
 
 shell_renew
-naive
-

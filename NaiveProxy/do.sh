d5a31386c427b41f4a790e185c3e9f461af5c87c
imajeason <imajeason@gmail.com>
2022-11-02 16:51:59 +0800

    fix

--- NaiveProxy/do.sh
+++ NaiveProxy/do.sh
@@ -1,7 +1,7 @@
 
 #! /bin/bash
 shell_renew(){
-    curl -o /root/.naive.sh https://raw.githubusercontent.com/imajeason/nas_tools/main/NaiveProxy/install.sh 
+    curl -o /root/.naive.sh https://raw.githubusercontent.com/imajeason/nas_tools/main/NaiveProxy/naive.sh 
     chmod +x /root/.naive.sh
     ln -s /root/.naive.sh /usr/bin/naive
     echo
--- NaiveProxy/naive.sh
+++ NaiveProxy/naive.sh
@@ -822,7 +822,7 @@ cert_renew(){
 shell_renew(){
     curl -o /root/.naive.sh https://raw.githubusercontent.com/imajeason/nas_tools/main/NaiveProxy/install.sh 
     chmod +x /root/.naive.sh
-    ln -s /root/.naive.sh /usr/bin/naive
+    # ln -s /root/.naive.sh /usr/bin/naive
 }
 
 show_config() {
